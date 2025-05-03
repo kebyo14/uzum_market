4d8ede3a091e9215809398bdfe5e23ba8eb1836e
Anvarov21 <pubgmobileummaaa1@gmail.com>
2025-05-03 18:28:47 +0500

    bosilish qismi tayyor

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('product/<int:pk>/',views.product_info,name='product_info')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Product
 
 def home(request):
     products = Product.objects.all()
     return render(request, 'uzum.html', {'products': products})
 
+def product_info(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    related_products = Product.objects.exclude(pk=pk)[:5]  # Пример: другие товары
+    return render(request, 'product_info.html', {
+        'product': product,
+        'products': related_products,  # для product_cards.html
+    })
