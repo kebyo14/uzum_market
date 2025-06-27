cbc906233b1246cfa624e0d94e47135e2dfa562c
Anvarov21 <pubgmobileummaaa1@gmail.com>
2025-06-27 19:48:08 +0500

    in admin updated control system

--- myapp/models.py
+++ myapp/models.py
@@ -14,43 +14,18 @@ class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    image = models.ImageField(upload_to='products/', verbose_name="Изображение")
+    image = models.ImageField(upload_to='products/', verbose_name="Изображение", blank=True, null=True)
     category = models.ForeignKey(
-        Category,
+        'Category',
         on_delete=models.CASCADE,
         related_name='products',
         verbose_name="Категория",
-        null=True,
-        blank=True
+        blank=True,
+        null=True
     )
 
     def __str__(self):
         return self.name
-
-    name = models.CharField(max_length=255, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    image = models.ImageField(upload_to='products/', verbose_name="Изображение")
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        related_name='products',
-        verbose_name="Категория",
-        null=True,  # временно допускаем пустое значение
-        blank=True
-    )
-
-    def __str__(self):
-        return self.name
-    name = models.CharField(max_length=255, verbose_name="Название")
-    description = models.TextField(verbose_name="Описание")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    image = models.ImageField(upload_to='products/', verbose_name="Изображение")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name="Категория")
-
-    def __str__(self):
-        return self.name
-    
     
 
 class Polzovatel(models.Model):
